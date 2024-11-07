f1b75eed1aab1eb53d6c2d3fe17aab4ff55938e0
Fadi Al Eliwi <fadieliwi@gmail.com>
2024-11-07 16:17:23 +0100

    DEMO

---
+++ Activity Kit DEMO/Activity Kit DEMO/Activity_Kit_DEMOApp.swift
@@ -0,0 +1,17 @@
+//
+//  Activity_Kit_DEMOApp.swift
+//  Activity Kit DEMO
+//
+//  Created by Fadi Al Eliwi on 07.11.24.
+//
+
+import SwiftUI
+
+@main
+struct Activity_Kit_DEMOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
